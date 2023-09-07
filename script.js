d78e59460805dddbd61cf050f5083bbe74f5603f
Mr. E <mr.e@Katrinas-iMac.local>
2023-09-07 00:45:36 -0400

    added script.js

---
+++ script.js
@@ -0,0 +1,84 @@
+const myButton = document.getElementbyId("button");
+
+myButton.addEventListener("click", function() {
+//code to be executed when the button is clicked
+
+
+});
+
+
+
+const questions = [
+
+{
+    question: "What does the DOM stand for in Javascript?",
+    options: {
+    a: "Document Object Model",
+    b: "Dynamic Object Manipulation",
+    c: "Data Output Mechanism", 
+    d: "Document Ordering Model",
+    },
+    correctAnswer: "a",
+},
+{
+    question:"Which keyword is used to declare a variable in Javascript with block scope?",
+    options: {
+        a: "var",
+        b: "let",
+        c: "const",
+        d: "function",
+    },
+    correctAnswer: "b",
+}
+{
+    question:"What does NaN in JavaScript stand for?",
+    options: {
+        a:"Not a number",
+        b:"Negative and Negative",
+        c:"Null and None",
+        d:"No Argument Needed"
+    },
+    correctAnswer: "a",
+}
+{
+    question:"What is the purpose of the addEventListener method in Javascript?",
+    options: {
+        a:"to perform mathematical caculations",
+        b:"to add styles to HTML elements",
+        c:"to handle events like clicks and keypresses",
+        d:"to create new HTML elements"
+    },
+    correctAnswer: "c",
+}
+{
+    question:"What is the purpose of the typeof operator in Javascript?",
+    options: {
+        a:"to check if a variable is defined",
+        b:"to determine the data type of a value",
+        c:"to convert a string to a number",
+        d:"to create a new variable",
+    },
+    correctAnswer: "b",
+}
+{
+    question:"Which of the following is used to iterate over the properties of an object in JavaScript?",
+    options: {
+        a:"for loop",
+        b:"while loop",
+        c:"forEach loop",
+        d:"for...in loop",
+    },
+    correctAnswer: "d",
+}
+{
+    question:"What is the purpose of the parseInt function in JavaScript?",
+    options: {
+        a:"to check if a string is empty",
+        b:"to convert a string to an integer",
+        c:"to round a floating-point number",
+        d:"to add two numbers together"
+    },
+    correctAnswer: "b",
+}
+
+];
